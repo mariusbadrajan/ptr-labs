94659ebcde1600f37b06b2a2d16e341983a434f4
Marius <marius.badrajan@mail.ru>
2023-02-01 11:02:53 +0200

    Lab1 CP1 Add Hello PTR

---
+++ src/main/scala/lab1/Main.scala
@@ -0,0 +1,8 @@
+package org.marius
+package lab1
+
+object Main {
+  def main(args: Array[String]) = {
+    println("Hello PTR")
+  }
+}
--- src/main/scala/main.scala
+++
@@ -1,6 +0,0 @@
-package org.marius
-
-@main
-def main(): Unit = {
-  println("Hello world!")
-}
